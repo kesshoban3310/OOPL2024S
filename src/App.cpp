10d57d4260ae1557ba655566c9ad7524a443bfe6
Eric <ericncnl3742@gmail.com>
2024-03-13 10:41:23 +0800

    update state machine and fix tilemap code

--- src/App.cpp
+++ src/App.cpp
@@ -10,7 +10,6 @@ void App::Start() {
 
     LOG_TRACE("Start");
 
-
     // if m_StartBackground not loaded
     if (!m_StartBackground) {
         m_StartBackground = std::make_shared<AnimatedObject>(
@@ -20,51 +19,57 @@ void App::Start() {
         m_StartBackground->SetScale({3, 3});
         m_Root.AddChild(m_StartBackground);
     }
-    m_converter = std::make_shared<Txtconverter>(RESOURCE_DIR "/Map/test.txt");
+
     // render windows
     m_Root.Update();
 
     // detect enter key to turn next phase
     if (Util::Input::IsKeyUp(Util::Keycode::RETURN)) {
-        m_CurrentState = State::UPDATE;
+        m_CurrentState = State::GAME_STAGE_SELECT;
 
         // free resource
         m_Root.RemoveChild(m_StartBackground);
         m_StartBackground.reset();
     }
-
-
-
 }
 
-void App::Update() {
+void App::GameStageSelect() {
+    m_CurrentState = State::GAME_STAGE;
+}
+
+void App::GameStage() {
     /*
-     * Initial Map.
+     * Initialize Map.
      */
-    std::vector<std::vector<int>> a(16,std::vector<int>(14,3)); //Test spawning map.
-    m_Map = std::make_shared<Backgroundmap>(m_converter->convert(),"Bomb Man Stage");
-    m_Map->SetImageToMap(true);
-    m_Root.AddChildren(m_Map->GetChildren());
-
-
-    if(Util::Input::IsKeyPressed(Util::Keycode::LEFT)) {
-
+    if (!m_converter) {
+        m_converter =
+            std::make_shared<Txtconverter>(RESOURCE_DIR "/Map/test.txt");
     }
-    if(Util::Input::IsKeyPressed(Util::Keycode::RIGHT)) {
-
-
+    if (!m_Map) {
+        // Test spawning map.
+        std::vector<std::vector<int>> a(16, std::vector<int>(14, 3));
+        m_Map = std::make_shared<Backgroundmap>(m_converter->convert(),
+                                                "Bomb Man Stage");
+        m_Map->SetImageToMap(true);
+        m_Root.AddChildren(m_Map->GetChildren());
     }
-    if(Util::Input::IsKeyPressed(Util::Keycode::UP)) {
 
-    }
-    if(Util::Input::IsKeyPressed(Util::Keycode::DOWN)) {
-
-    }
+    // render windows
     m_Root.Update();
 
+    // TODO: win game -> ENDING_ANIMATION
+    // lose game -> GAME_LOSE
     if (Util::Input::IsKeyUp(Util::Keycode::ESCAPE) || Util::Input::IfExit()) {
         m_CurrentState = State::END;
     }
+}
+
+void App::GameLose() {
+    m_CurrentState = State::ENDING_ANIMATION;
+}
+
+void App::EndingAnimation() {
+    m_CurrentState = State::END;
 }
 
 void App::End() { // NOLINT(this method will mutate members in the future)
--- src/Component/Backgroundmap.cpp
+++ src/Component/Backgroundmap.cpp
@@ -1,39 +1,37 @@
-//
-// Created by User on 2024/3/6.
-//
-
 #include "Component/Backgroundmap.hpp"
 #include "Component/Tilemap.hpp"
 #include "Util/Logger.hpp"
 
-Backgroundmap::Backgroundmap(const std::vector<std::vector<int>> &v,const std::string &stage) {
+Backgroundmap::Backgroundmap(const std::vector<std::vector<int>> &v,
+                             const std::string &stage) {
     this->map_num = v;
     this->stage = stage;
 }
 
-void Backgroundmap::SetImageToMap(const bool &visable) {
-    int n = map_num.size(), m = map_num[0].size();
-    std::string Image_path = RESOURCE_DIR"/Picture/Tiles/" + stage;
-    for(int i=0;i<n;i++){
-        std::vector<std::shared_ptr<ImageObject> > tmp;
-        for(int j=0;j<m;j++){
-            std::shared_ptr<ImageObject> Block = std::make_shared<ImageObject>(Image_path+"/BackGround/"+std::to_string(map_num[i][j])+".png");
-            Block->SetScale({3,3});
+void Backgroundmap::SetImageToMap(const bool &visible) {
+    std::string Image_path = RESOURCE_DIR "/Picture/Tiles/" + stage;
+    for (int i = 0; i < (int)map_num.size(); i++) {
+        std::vector<std::shared_ptr<ImageObject>> tmp;
+        for (int j = 0; j < (int)map_num[i].size(); j++) {
+            std::shared_ptr<ImageObject> Block = std::make_shared<ImageObject>(
+                Image_path + "/BackGround/" + std::to_string(map_num[i][j]) +
+                ".png");
+            Block->SetScale({3, 3});
             Block->SetZIndex(-10);
-            Block->SetPosition(Tilemap::GetTilePosition(j,i));
-            Block->SetVisible(visable);
+            Block->SetPosition(Tilemap::GetTilePosition(i, j));
+            Block->SetVisible(visible);
             tmp.push_back(Block);
         }
         Map.push_back(tmp);
     }
 }
-void Backgroundmap::Move(const int dx,const int dy) {
+void Backgroundmap::Move(const int dx, const int dy) {
     int n = map_num.size(), m = map_num[0].size();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             auto Image_block = Map[i][j];
             auto pos = Image_block->GetPosition();
-            Image_block->SetPosition({pos.x+dx,pos.y+dy});
+            Image_block->SetPosition({pos.x + dx, pos.y + dy});
         }
     }
 }
