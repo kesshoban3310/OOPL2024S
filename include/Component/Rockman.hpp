934c8ea743c9b524eec297c1feb1331f0d08f56c
arod571431 <nick930212@gmail.com>
2024-03-17 23:48:08 +0800

    Add Rock Man Death Picture

--- include/Component/Rockman.hpp
+++ include/Component/Rockman.hpp
@@ -7,6 +7,9 @@
 
 class Rockman{
 public:
+    /**
+     * @brief Constructor.
+     */
     Rockman();
     /**
      * @brief Initialize the object.
@@ -14,19 +17,32 @@ public:
      */
     void Initialize();
     /**
-     * @brief Set Object can see or cannot see.
+     * @brief Set Object different movement.
+     * @param idx for the character's index.
      */
     void Setvisable(const int &idx);
-
-
+    /**
+     * @brief collect all object in character.
+     * @return All object in character.
+     */
     [[nodiscard]]std::vector<std::shared_ptr<Util::GameObject>> Getchildren() const{
         std::vector<std::shared_ptr<Util::GameObject>> children;
         for(auto i:character)
             children.push_back(i);
         return children;
     }
+    /**
+     * @brief Set object position.
+     */
     void Setposition(glm::vec2 position);
+    /**
+     * @brief Get object position.
+     * @return Object's postition.
+     */
     glm::vec2 Getposition();
+    /**
+     * @brief Make Rockman Move.
+     */
     void move();
 private:
     std::vector<std::shared_ptr<AnimatedObject>> character;
