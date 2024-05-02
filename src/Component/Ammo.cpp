f4aae78e39a46b25e1d59f9f14ed800a25e9553d
arod571431 <nick930212@gmail.com>
2024-05-02 21:51:06 +0800

    Modify Ammo API And Moving Function

--- src/Component/Ammo.cpp
+++ src/Component/Ammo.cpp
@@ -5,7 +5,8 @@
 #include "Util/Keycode.hpp"
 #include "Util/Time.hpp"
 
-Ammo::Ammo(glm::vec2 position, glm::vec2 speed, std::string imagepath,glm::vec2 ammosize) {
+Ammo::Ammo(glm::vec2 position, glm::vec2 speed, std::string imagepath,
+           glm::vec2 ammosize) {
     this->path = imagepath;
     this->speed = speed;
     ammo = std::make_shared<ImageObject>(path);
@@ -16,23 +17,26 @@ Ammo::Ammo(glm::vec2 position, glm::vec2 speed, std::string imagepath,glm::vec2 
     ammo->SetVisible(true);
 
     final_position = position;
-    if(speed.x!=0){
-        final_position.x+=(speed.x/abs(speed.x))*768;
+    if (speed.x != 0) {
+        final_position.x += (speed.x / abs(speed.x)) * 768;
     }
-    if(speed.y!=0){
-        final_position.y+=(speed.y/abs(speed.y))*720;
+    if (speed.y != 0) {
+        final_position.y += (speed.y / abs(speed.y)) * 720;
     }
-    colliderbox = std::make_shared<Collider>(initial_position,ammosize,glm::vec2 {0,0});
+    colliderbox =
+        std::make_shared<Collider>(initial_position, ammosize, glm::vec2{0, 0});
 }
 bool Ammo::Outofrange(glm::vec2 camerapos) { // need to add collision box;
     bool Xoverlap = false;
     bool Yoverlap = false;
-    Xoverlap = camerapos.x-384 >= initial_position->x || camerapos.x+384 <= initial_position->x;
-    Yoverlap = camerapos.y-360 >= initial_position->y || camerapos.y+360 <= initial_position->y;
+    Xoverlap = camerapos.x - 384 >= initial_position->x ||
+               camerapos.x + 384 <= initial_position->x;
+    Yoverlap = camerapos.y - 360 >= initial_position->y ||
+               camerapos.y + 360 <= initial_position->y;
     return Xoverlap || Yoverlap;
 }
 void Ammo::Behavior() {
-    initial_position->x += (Util::Time::GetDeltaTime()*1000) * speed.x * 0.05;
-    initial_position->y += (Util::Time::GetDeltaTime()*1000) * speed.y * 0.05;
+    initial_position->x += Util::Time::GetDeltaTimeMs() * speed.x * 0.05;
+    initial_position->y += Util::Time::GetDeltaTimeMs() * speed.y * 0.05;
     ammo->SetPosition(*initial_position);
 }
