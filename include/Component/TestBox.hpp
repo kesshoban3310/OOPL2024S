ca74451f90adbd4139760f569c7e976bb524a4d1
arod571431 <nick930212@gmail.com>
2024-05-08 02:11:48 +0800

    TestBox

--- include/Component/TestBox.hpp
+++ include/Component/TestBox.hpp
@@ -7,12 +7,16 @@ class TestBox {
 public:
     TestBox(glm::vec2 pos, glm::vec2 sca);
     void Move();
+    void SetPosition(glm::vec2 pos){
+        *position = pos;
+        Object->SetPosition(pos);
+    }
     std::shared_ptr<Util::GameObject> Getchild() {
         std::shared_ptr<Util::GameObject> box = Object;
         return box;
     }
     std::shared_ptr<Collider> Getcollisonbox() { return collisonbox; }
-
+    glm::vec2 GetPosition() {return *position;}
 private:
     glm::vec2 scale;
     std::shared_ptr<glm::vec2> position;
