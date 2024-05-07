cad10768854cb5b49e49c780a8117137065f09a7
arod571431 <nick930212@gmail.com>
2024-05-08 02:22:53 +0800

    Modify Object

--- include/PhaseStage.hpp
+++ include/PhaseStage.hpp
@@ -6,6 +6,9 @@
 #include "Component/TileMap.hpp"
 #include "Component/Blaster.hpp"
 #include "Component/ScrewDriver.hpp"
+#include "Component/Bombombomb.hpp"
+#include "Component/HealthBar.hpp"
+#include "Component/OctopusBattery.hpp"
 #include "IncludeComponent.hpp"
 
 class PhaseStage final : public Phase {
@@ -25,9 +28,12 @@ private:
     std::shared_ptr<Rockman> m_Rockman;
     std::shared_ptr<Scorebar> m_Scorebar;
     std::shared_ptr<TestBox> m_Testbox;
+    std::shared_ptr<HealthBar> m_RockmanHealthBar;
     std::queue<std::shared_ptr<Ammo>> m_Magazine; //Store Rockman's Ammo.
     std::vector<std::shared_ptr<Blaster>> m_Blaster;
     std::vector<std::shared_ptr<Screwdriver>> m_Screwdriver;
+    std::vector<std::shared_ptr<OctopusBattery>> m_OctopusBattery;
+    std::vector<std::shared_ptr<Bombombomb>> m_Bombombomb;
     SceneManager m_SceneManager;
 
     void ReloadMagazine(App *app); //Update Every Ammo In The Stage.
