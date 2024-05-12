2dc445ecbc20623b53e8d6464b58d7c33d95cc78
arod571431 <nick930212@gmail.com>
2024-05-13 00:34:53 +0800

    Get Label

--- include/Component/Ammo.hpp
+++ include/Component/Ammo.hpp
@@ -5,16 +5,26 @@
 #include "ImageObject.hpp"
 #include "pch.hpp"
 
+enum class AmmoType { ROCKMAN, ENEMY };
+
 class Ammo {
 public:
     Ammo(glm::vec2 position, glm::vec2 speed, std::string imagepath,
-         glm::vec2 ammosize);
+         glm::vec2 ammosize, AmmoType type);
     void Behavior();
     bool Outofrange(glm::vec2 camerapos);
     [[nodiscard]] std::shared_ptr<Util::GameObject> GetChild() {
         std::shared_ptr<Util::GameObject> Object = ammo;
         return Object;
     }
+
+    [[nodiscard]] Collider GetCollider() { return *colliderbox; }
+
+    [[nodiscard]] AmmoType GetAmmoType() { return type; }
+
+    void MarkForRemoval() { marked_for_removal = true; }
+
+    [[nodiscard]] bool IsMarkedForRemoval() const { return marked_for_removal; }
 
 private:
     std::shared_ptr<ImageObject> ammo;
@@ -24,6 +34,8 @@ private:
     glm::vec2 speed;
     std::string path;
     double delta;
+    bool marked_for_removal = false;
+    AmmoType type;
 };
 
 #endif
