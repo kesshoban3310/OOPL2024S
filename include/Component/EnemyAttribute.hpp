e9761604482d0dd2904db508e8b764f4dac1dbf0
arod571431 <nick930212@gmail.com>
2024-05-13 00:31:45 +0800

    Add Enemy For Inheritence

--- include/Component/EnemyAttribute.hpp
+++ include/Component/EnemyAttribute.hpp
@@ -1,29 +1,80 @@
 #ifndef ENEMY_ATTRIBUTE_HPP
 #define ENEMY_ATTRIBUTE_HPP
 
+#include "Component/Collider.hpp"
+#include "Component/ImageObject.hpp"
+#include "Util/GameObject.hpp"
 #include "glm/vec2.hpp"
 #include "pch.hpp"
-#include "Util/GameObject.hpp"
+#include <memory>
 
 /**
  * @brief This class is an object pool for every enemy man.
  * Including and inherit the attribute you want.
  */
-class Enemy{
+class Enemy {
 public:
+    enum class HurtState { // To see if it is harmful or unharmful.
+        INVINCIBLE,        // Unharmful.
+        COWARDLY,          // Harmful.
+    };
+    enum class LifeState { // To see if it is life or dead.
+        LIFE,
+        DEAD,
+    };
+    Enemy(glm::vec2 pos, std::string path, int hp, bool visable,
+          glm::vec2 collidersize, HurtState state,
+          LifeState lifeState = LifeState::LIFE, glm::vec2 scale = {3, 3});
+    virtual ~Enemy() = default;
     virtual void Behavior(glm::vec2 position) = 0;
-    virtual void Initialize() = 0;
+
+    virtual glm::vec2 GetPosition();
+    virtual void SetPosition(glm::vec2 pos);
+
+    virtual void SetHealth(int health);
+    virtual int GetHealth();
+
+    virtual void SetVisable(bool visable); // disable enemy.
+    virtual bool GetVisable();             // Get Visable
+
+    virtual void SetImage(std::string path);
+
+    virtual HurtState GetHurtState();
+    virtual void SetHurtState(HurtState hurtstate);
+
+    virtual LifeState GetLifeState();
+    virtual void SetLifeState(LifeState lifestate);
+
+    [[nodiscard]] Collider GetCollider() {
+        return *Hitbox;
+    } // Get Enemy Collider.
+    [[nodiscard]] std::shared_ptr<Util::GameObject> GetChild() {
+        std::shared_ptr<Util::GameObject> object = Object;
+        return object;
+    }
+
+protected:
+    std::shared_ptr<ImageObject> Object;
+    std::shared_ptr<glm::vec2> Position;
+    std::shared_ptr<Collider> Hitbox;
+    HurtState Hurt;
+    glm::vec2 Scale;
+    int Health;
+    bool Visable;
+    LifeState Life;
 };
+
 class Attackable {
 public:
     virtual void Attack() = 0;
 };
-class Physic{
+class Physic {
 public:
     virtual void PhysicEngine() = 0;
 };
-class Shootable{
+class Shootable {
 public:
     virtual void Shoot() = 0;
 };
+
 #endif
---
+++ src/Component/EnemyAttribute.cpp
@@ -0,0 +1,64 @@
+//
+// Created by User on 2024/5/12.
+//
+
+#include "Component/EnemyAttribute.hpp"
+
+Enemy::Enemy(glm::vec2 pos, std::string path, int hp, bool visable,
+             glm::vec2 collidersize, Enemy::HurtState state,
+             Enemy::LifeState lifeState, glm::vec2 scale) {
+    this->Health = hp;
+    this->Life = lifeState;
+    this->Hurt = state;
+    this->Visable = visable;
+    this->Position = std::make_shared<glm::vec2>(pos);
+    this->Hitbox =
+        std::make_shared<Collider>(Position, collidersize, glm::vec2{0, 0});
+    this->Object = std::make_shared<ImageObject>(path);
+    this->Scale = scale;
+    Object->SetPosition(pos);
+    Object->SetScale(Scale);
+    Object->SetVisible(visable);
+    Object->SetZIndex(70);
+}
+
+void Enemy::SetHealth(int health) {
+    Health = health;
+}
+int Enemy::GetHealth() {
+    return Health;
+}
+
+void Enemy::SetVisable(bool visable) {
+    Visable = visable;
+    Object->SetVisible(Visable);
+}
+bool Enemy::GetVisable() {
+    return Visable;
+}
+
+void Enemy::SetImage(std::string path) {
+    Object->SetImage(path);
+}
+
+Enemy::HurtState Enemy::GetHurtState() {
+    return Hurt;
+}
+void Enemy::SetHurtState(HurtState hurtstate) {
+    Hurt = hurtstate;
+}
+
+Enemy::LifeState Enemy::GetLifeState() {
+    return Life;
+}
+void Enemy::SetLifeState(LifeState lifestate) {
+    Life = lifestate;
+}
+
+glm::vec2 Enemy::GetPosition() {
+    return *Position;
+}
+void Enemy::SetPosition(glm::vec2 pos) {
+    *Position = pos;
+    Object->SetPosition(pos);
+}
